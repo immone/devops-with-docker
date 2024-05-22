d252be9d80121dfb2595de9bd91b4c39aa625216
immone <antti.imm1n@gmail.com>
2024-05-22 11:20:00 +0300

    Add 3.4

--- Part 3/3.2/builder.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-git clone https://github.com/${1}.git
-cd ${1##*/} 
-docker build . -t bash-app
-docker login 
-docker tag bash-app ${2}
-docker push ${2}
---
+++ Part 3/3.3/builder.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+echo "Cloning repo from https://github.com/$1.git.."
+git clone https://github.com/$1.git
+
+echo "Changing folder to ${1##*/}.."
+cd ${1##*/} 
+
+echo "Logging in.."
+docker build . -t bash-app
+docker login 
+
+echo "Tagging $2.."
+docker tag bash-app $2
+
+echo "Pushing $2 to docker.."
+docker push $2
+
+echo "Done!"
